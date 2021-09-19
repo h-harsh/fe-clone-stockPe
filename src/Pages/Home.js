b6ba58a4901ba9f854533b777e8290574938afcb
h-harsh <porwalharshp@gmail.com>
2021-09-19 14:00:16 +0530

    basic str setup

---
+++ src/Pages/Home.js
@@ -0,0 +1,22 @@
+import { Main } from "../Components/Main/main";
+import { Navbar } from "../Components/Nav Bar/navBar";
+
+export const Home = () => {
+  return (<>
+    <Navbar/>
+    <Main/>
+  </>);
+};
+
+// {
+//   /* <NavBar/>
+//         <Main/>
+//         <BelowMain/>
+//         <MobileImage/>
+//         <Objective1/>
+//         <MobileImage2/>
+//         <Objective2/>
+//         <MobileImage3/>
+//         <End/>
+//         <Footer/> */
+// }
